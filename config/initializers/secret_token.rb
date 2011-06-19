154fb76e38a2c84f2d0b743f13475f5bf9e7c6b4
John Hinnegan <john.hinnegan@gmail.com>
2011-06-19 14:51:11 -0700

    fresh project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleScaffoldReference::Application.config.secret_token = '9cff88430a481d68035153c0974beb5eb73721e06d7fa18977b67d4ebdd37d906d80d327cc6122722b79c1b4caa2f4590db4fdca5ef2f23fc375e8f0db2c39f3'
